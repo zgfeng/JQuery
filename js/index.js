39a1792621b085c0a1864d0e7721a839afc3452e
zgf <folle@live.cn>
2016-03-14 17:05:43 +0800

    日历1

--- js/index.js
+++ js/index.js
@@ -17,7 +17,20 @@ $(function(){
     });
 
     $('#reg').buttonset();
-    $('#date').datepicker();
+    $('#date').datepicker({
+        dateFormat : 'yy-mm-dd',
+        //dayNames : ['星期日','星期一','星期二','星期三','星期四','星期五','星期六'],
+		//dayNamesShort : ['星期日','星期一','星期二','星期三','星期四','星期五','星期六'],
+		dayNamesMin : ['日','一','二','三','四','五','六'],
+		monthNames : ['一月','二月','三月','四月','五月','六月','七月','八月','九月','十月','十一月','十二月'],
+		//monthNamesShort : ['一','二','三','四','五','六','七','八','九','十','十一','十二'],
+		altField : '#abc',
+		altFormat : 'dd/mm/yy',
+		appendText : '日历',
+		showWeek : true,
+		weekHeader : '周',
+		firstDay : 1,
+    });
 
     $('#reg input[title]').tooltip({
         show : false,
